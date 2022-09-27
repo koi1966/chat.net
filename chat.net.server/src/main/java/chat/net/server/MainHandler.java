7c8c69b36830c9985652b13d6266768e9650fb64
Oleg Klimchuk <koi196621@gmail.com>
2022-09-27 17:15:29 +0300

    Begin

---
+++ chat.net.server/src/main/java/chat/net/server/MainHandler.java
@@ -0,0 +1,21 @@
+package chat.net.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class MainHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+}
---
+++ chat.net.server/src/main/java/chat/net/server/ServerApp.java
@@ -0,0 +1,31 @@
+package chat.net.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        EventLoopGroup boosGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                        }
+                    });
+            ChannelFuture future = b.bind(8189).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
