746446d24b24af642889098e666cc4e48e0a30fa
koi19 <koi196621@gmail.com>
2022-09-28 07:49:05 +0300

    Begin

--- chat.client/src/main/java/chat/net/client/Main.java
+++ chat.client/src/main/java/chat/net/client/Main.java
@@ -1,4 +1,21 @@
 package chat.net.client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("\\client.fxml"));
+        primaryStage.setTitle("Hello Chat Client");
+        primaryStage.setScene(new Scene(root,400,400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
